2ffbe02cc5a9754dfbd9227e4accfe8b6446276b
IAiAye <holmesian16@gmail.com>
2023-02-08 01:30:57 +0900

    Unit 4-2 edited

--- 04-2_class/048_problem_2.cpp
+++ 04-2_class/048_problem_2.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+#include <cmath>
 
+using namespace std;
 class Point
 {
+int x, y;
 
 public:
-    int x, y;
 
     Point(int pos_x, int pos_y)
     {
         x = pos_x;
         y = pos_y;
     }
+    int GetX() const {return x;}
+    int GetY() const {return y;}
+
     void ShowPoint()
     {
-        std::cout << "(" << x << "," << y << ")" << std::endl;
+        cout << "(" << x << "," << y << ")" << endl;
     }
 };
 
@@ -21,37 +26,88 @@ class Geometry
 {
 private:
     int num_points;
-    Point* point_array[100];
+    Point* point_array[100]; //Point 클래스의 pointer 생성
 public:
     Geometry()
     {
         num_points = 0;
     }
+    ~Geometry() // 소멸자
+    {
+        for (int i = 0; i < num_points; i++)
+        {
+            delete point_array[i];
+        }
+    }
 
     void AddPoint(const Point &point)
     {
-        point_array[num_points ++] = new Point(point.x, point.y);
+        point_array[num_points ++] = new Point(point.GetX(), point.GetY());
     }
-    void PrintDistance()
-    {
-        std::cout << point_array[0] << std::endl;
-        std::cout << point_array[1] << std::endl;
-    }
-
-
+    void PrintDistance();
+    void PrintNumMeets();
 };
+
+void Geometry::PrintDistance()
+{
+    for (int i = 0; i < num_points - 1; i++)
+    {
+        for (int j = i + 1; j < num_points; j++)
+        {
+            cout << i + 1 << "번째 점과 " << j + 1 << "번째 점의 거리는 " 
+            << sqrt(pow(point_array[i]->GetX() - point_array[j]->GetX(),2) + pow(point_array[i]->GetY() - point_array[j]->GetY(),2))
+            << "입니다." << endl;
+        }
+           
+    }
+}
+void Geometry::PrintNumMeets()
+{
+    if (num_points < 4)
+    {
+        cout << "점의 개수가 교점을 생성하기에 적습니다." <<endl;
+    }
+    else
+    {
+        for (int i = 0; i < num_points - 3; i++)
+        {
+            for (int j = 0; j < num_points - 2; j++)
+            {
+                for (int k = 0; k < num_points - 1; k++)
+                {
+                    for (int m = 0; m < num_points; m++)
+                    {
+                        // 교점의 정의가 모호하여 포기.
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
 
 int main()
 {
-    Point point1(2,3);
-    Point point2(3,2);
+    Point pos1(0,0);
+    Point pos2(3,4);
+    Point pos3(12,5);
+    Point pos4(16,9);
 
-    point1.ShowPoint();
-    point2.ShowPoint();
+    pos1.ShowPoint();
+    pos2.ShowPoint();
+    pos3.ShowPoint();
+    pos4.ShowPoint();
 
     Geometry geo;
-    geo.AddPoint(point1);
-    geo.AddPoint(point2);
+    geo.AddPoint(pos1);
+    geo.AddPoint(pos2);
+    geo.AddPoint(pos3);
+    geo.AddPoint(pos4);
 
     geo.PrintDistance();
 
