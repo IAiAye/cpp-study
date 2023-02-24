b867bfb92eb1fdf65a6826c9b4690b1bfa5deda6
IAiAye <holmesian16@gmail.com>
2023-02-24 23:44:10 +0900

    Unit 4-6 edited

--- 04-6_explicit_mutable/0425_explicit.cpp
+++ 04-6_explicit_mutable/0425_explicit.cpp
@@ -69,11 +69,11 @@ int main()
     DoSomethingWithString(MyString("abc")); // 다음은 당연히 클래스의 객체를 만들어서 넣었으므로 동작한다.
     DoSomethingWithString("DEFE"); // 하지만 다음은 클래스를 표시하지 않았지만, MyString(const char* str)라는 생성자가 있으므로 자동으로 변환하여 대입하게 된다.
     // 이를 암시적 변환 (implicit conversion)이라 부른다.
-    DoSomethingWithString(4); // 하지만 만약 우리가 실수로 문자 4를 넣기를 원했으나 숫자 4를 넣어도 자동으로 변환하여 생성자를 실행시켜준다.
+    //DoSomethingWithString(4); // 하지만 만약 우리가 실수로 문자 4를 넣기를 원했으나 숫자 4를 넣어도 자동으로 변환하여 생성자를 실행시켜준다.
 
     //이렇게 암시적 변환을 하지 못하게 명시해주는 방법이 explicit이다.
 
     MyString s1 = "abc"; // 다음과 같이 복사생성자의 형태로 호출되는 것도 막아준다.
-    MyString s2 = 5;
+    //MyString s2 = 5;
     MyString s3(5); // 다음과 같이 명시적으로 생성자를 부를때만 허용해준다.
 }
